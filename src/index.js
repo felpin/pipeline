97dad0ebea96c85a2a969185316f19ab4901c2b1
Felipe Pinheiro <felipe.pinheiro.90@hotmail.com>
2019-08-07 01:19:20 -0300

    Create a theme and a global style

--- src/index.js
+++ src/index.js
@@ -1,4 +1,36 @@
+/* eslint-disable react/jsx-filename-extension */
 import 'normalize.css';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
-ReactDOM.render(null, document.getElementById('root'));
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Montserrat', sans-serif;
+  }
+`;
+
+const theme = {
+  color: {
+    blue: '#0072ce',
+    dark: '#333f48',
+    green: '#18ad18',
+    light: '#979ea2',
+    red: '#f9423a',
+  },
+  fontSize: {
+    medium: '16px',
+    small: '12px',
+  },
+  fontWeight: {
+    bold: 600,
+    regular: 400,
+  },
+};
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+  </ThemeProvider>,
+  document.getElementById('root')
+);
