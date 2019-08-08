939c78926edeabfcf458836c7cc6f29f27447214
Felipe Pinheiro <felipe.pinheiro.90@hotmail.com>
2019-08-08 00:34:58 -0300

    Add the user reducer to the store

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,10 @@
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
 import thunk from 'redux-thunk';
 
 import pipeline from './pipeline';
+import user from './user';
 
-export default createStore(pipeline, composeWithDevTools(applyMiddleware(thunk)));
+const rootReducer = combineReducers({ pipeline, user });
+
+export default createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
