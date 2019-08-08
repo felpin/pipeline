ff091f50d3e46154cb57371364e11033bde5d5d7
Felipe Pinheiro <felipe.pinheiro.90@hotmail.com>
2019-08-07 21:42:08 -0300

    Add i18n to the project through react-18next

---
+++ src/i18n/index.js
@@ -0,0 +1,19 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
+
+import en from './resources/en.json';
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    debug: process.env.NODE_ENV === 'development',
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+    resources: { en },
+  });
+
+export default i18n;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
+import './i18n';
+
 const GlobalStyle = createGlobalStyle`
   body {
     background-color: #fafafa;
