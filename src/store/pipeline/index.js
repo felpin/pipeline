4e211f1b5af55bba09e053805fa1508b2b43e69a
Felipe Pinheiro <felipe.pinheiro.90@hotmail.com>
2019-08-08 01:11:53 -0300

    Create a fetch action and improve reducer

--- src/store/pipeline/index.js
+++ src/store/pipeline/index.js
@@ -1,1 +1,22 @@
-export default state => state;
+import pipelineService from '../../services/pipeline';
+import { login } from '../user';
+import { idSelector } from '../user/selectors';
+
+const FETCH = 'pipeline/pipeline/FETCH';
+
+export default (state = [], action) => {
+  switch (action.type) {
+    case `${FETCH}_FULFILLED`:
+      return action.payload.data;
+
+    default:
+      return state;
+  }
+};
+
+export const fetch = () => async (dispatch, getState) => {
+  await dispatch(login());
+
+  const id = idSelector(getState());
+  return dispatch({ type: FETCH, payload: pipelineService(id) });
+};
