c528ef36d92c45329801830647b3182d9d51ae50
Felipe Pinheiro <felipe.pinheiro.90@hotmail.com>
2019-08-08 01:07:08 -0300

    Create a selector for expiration time of token

--- src/store/user/selectors.js
+++ src/store/user/selectors.js
@@ -1,2 +1,10 @@
+import jwt from 'jsonwebtoken';
+import { createSelector } from 'reselect';
+
 export const idSelector = state => state.user.id;
 export const tokenSelector = state => state.user.token;
+
+export const expSelector = createSelector(
+  tokenSelector,
+  token => (token ? jwt.decode(token).exp : null)
+);
