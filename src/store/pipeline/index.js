29c1afd9971694df79584305214a2c69399d7b23
Felipe Pinheiro <felipe.pinheiro.90@hotmail.com>
2019-08-08 01:30:59 -0300

    Create an action to change status

--- src/store/pipeline/index.js
+++ src/store/pipeline/index.js
@@ -1,18 +1,38 @@
+import produce from 'immer';
+
 import pipelineService from '../../services/pipeline';
 import { login } from '../user';
 import { idSelector } from '../user/selectors';
 
+const CHANGE_STATUS = 'pipeline/pipeline/CHANGE_STATUS';
 const FETCH = 'pipeline/pipeline/FETCH';
 
-export default (state = [], action) => {
-  switch (action.type) {
-    case `${FETCH}_FULFILLED`:
-      return action.payload.data;
+export default (state = [], action) =>
+  produce(state, draft => {
+    switch (action.type) {
+      case CHANGE_STATUS: {
+        const { id, newStatus } = action.payload;
+        const itemToChange = draft.find(item => item.id === id);
+        itemToChange.status = newStatus;
+        break;
+      }
 
-    default:
-      return state;
-  }
-};
+      case `${FETCH}_FULFILLED`: {
+        action.payload.data.forEach(item => {
+          draft.push(item);
+        });
+        break;
+      }
+
+      default:
+        break;
+    }
+  });
+
+export const changeStatus = (id, newStatus) => ({
+  type: CHANGE_STATUS,
+  payload: { id, newStatus },
+});
 
 export const fetch = () => async (dispatch, getState) => {
   await dispatch(login());
