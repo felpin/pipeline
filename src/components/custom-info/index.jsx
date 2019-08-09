d478a89c7e9c175ed6aec921d26fb43d66eec9dc
Felipe Pinheiro <felipe@jungledevs.com>
2019-08-09 20:25:49 -0300

    Create a custom info component

---
+++ src/components/custom-info/index.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import Container from './container';
+import Date from './date';
+
+const CustomInfo = ({ date, Icon }) => (
+  <Container>
+    <Icon />
+    <Date>{date.slice(0, 10)}</Date>
+  </Container>
+);
+
+CustomInfo.propTypes = {
+  date: PropTypes.string.isRequired,
+  // TODO: Validate this as a React forward ref type
+  // eslint-disable-next-line react/forbid-prop-types
+  Icon: PropTypes.object.isRequired,
+};
+
+export default CustomInfo;
