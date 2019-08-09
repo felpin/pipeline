4c1ed3ff9a1b68cac09f0ab3ae18f800b8838ed5
Felipe Pinheiro <felipe@jungledevs.com>
2019-08-09 10:31:16 -0300

    Remove italic font-style from vertical ellipsis

--- src/components/column-card/vertical-ellipsis.js
+++ src/components/column-card/vertical-ellipsis.js
@@ -3,5 +3,6 @@ import styled from 'styled-components';
 export default styled.span`
   color: ${props => props.theme.color.dark};
   font-size: ${props => props.theme.fontSize.medium};
+  font-style: normal;
   font-weight: ${props => props.theme.fontWeight.regular};
 `;
