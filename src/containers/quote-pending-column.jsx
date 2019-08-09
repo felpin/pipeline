c9076416e38e9caa9fa2ea35358d8ff15e5fbb81
Felipe Pinheiro <felipe@jungledevs.com>
2019-08-09 18:23:18 -0300

    Create the quote pending column

---
+++ src/containers/quote-pending-column.jsx
@@ -0,0 +1,32 @@
+import React, { useCallback, useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
+
+import Column from '../components/column';
+import ColumnCard from '../components/column-card';
+import ColumnHeader from '../components/column-header';
+import { QUOTE_PENDING } from '../contants/pipeline-status';
+import { pipelineGroupedSelector } from '../store/pipeline/selectors';
+
+const QuotePendingColumn = () => {
+  const quotePendingItems = useSelector(pipelineGroupedSelector)[QUOTE_PENDING];
+  const { t } = useTranslation();
+
+  const header = useMemo(
+    () => (
+      <ColumnHeader
+        deals={quotePendingItems.length}
+        key="header"
+        showTotals={false}
+        title={t('quotePending')}
+      />
+    ),
+    [quotePendingItems.length, t]
+  );
+
+  const renderItem = useCallback(item => <ColumnCard client={item.client} id={item.id} />, []);
+
+  return <Column header={header} items={quotePendingItems} renderItem={renderItem} />;
+};
+
+export default QuotePendingColumn;
