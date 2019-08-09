af4e0b76a57ab1ffba4822b10472f31d290f0601
Felipe Pinheiro <felipe@jungledevs.com>
2019-08-09 20:49:08 -0300

    Add fill property on actions

--- src/components/column-card/bottom-right-container.js
+++ src/components/column-card/bottom-right-container.js
@@ -4,5 +4,6 @@ import TopRightContainer from './top-right-container';
 
 export default styled(TopRightContainer)`
   color: ${props => props.theme.color.blue};
+  fill: currentColor;
   grid-area: ${props => props.row} / 2;
 `;
