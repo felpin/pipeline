9832a923669a0548e35ea35ed0aa5a3f91113d3c
Felipe Pinheiro <felipe@jungledevs.com>
2019-08-09 21:17:36 -0300

    Add actions on items on quote ready column

--- src/containers/quote-ready-column.jsx
+++ src/containers/quote-ready-column.jsx
@@ -1,19 +1,44 @@
 import React, { useCallback, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
+import { ReactComponent as AcceptIcon } from '../assets/accept-button.svg';
+import { ReactComponent as InvoiceIcon } from '../assets/invoice.svg';
+import { ReactComponent as RefuseIcon } from '../assets/refuse-button.svg';
+import IconButtonSet from '../components/icon-button-set';
 import Column from '../components/column';
 import ColumnCard from '../components/column-card';
 import ColumnHeader from '../components/column-header';
 import { EUR, GBP } from '../contants/currency';
-import { QUOTE_READY } from '../contants/pipeline-status';
+import CustomInfo from '../components/custom-info';
+import { QUOTE_ACCEPTED, QUOTE_READY, QUOTE_REFUSED } from '../contants/pipeline-status';
+import { changeStatus } from '../store/pipeline';
 import { makePipelineStatusItemsSelector } from '../store/pipeline/selectors';
 
 const quoteReadyItemsSelector = makePipelineStatusItemsSelector(QUOTE_READY);
 
 const QuoteReadyColumn = () => {
+  const dispatch = useDispatch();
   const quoteReadyItems = useSelector(quoteReadyItemsSelector);
   const { t } = useTranslation();
+
+  const createActionItemData = useCallback(
+    item => {
+      const onAccept = () => {
+        dispatch(changeStatus(item.id, QUOTE_ACCEPTED));
+      };
+
+      const onRefuse = () => {
+        dispatch(changeStatus(item.id, QUOTE_REFUSED));
+      };
+
+      return [
+        { ariaLabel: t('acceptQuote'), children: <AcceptIcon />, onClick: onAccept },
+        { ariaLabel: t('refuseQuote'), children: <RefuseIcon />, onClick: onRefuse },
+      ];
+    },
+    [dispatch, t]
+  );
 
   const getTaxedTotal = useCallback(
     currency =>
@@ -39,9 +64,21 @@ const QuoteReadyColumn = () => {
     [quoteReadyItems.length, t, taxedTotalEur, taxedTotalGbp]
   );
 
-  const renderItem = useCallback(item => <ColumnCard client={item.client} id={item.id} />, []);
+  const renderItem = useCallback(
+    item => (
+      <ColumnCard
+        action={<IconButtonSet data={createActionItemData(item)} />}
+        client={item.client}
+        currency={item.currency}
+        customInfo={<CustomInfo date={item.updatedAt} Icon={InvoiceIcon} />}
+        id={item.id}
+        taxedTotal={item.taxedTotal}
+      />
+    ),
+    [createActionItemData]
+  );
 
   return <Column header={header} items={quoteReadyItems} renderItem={renderItem} />;
 };
 
-export default QuoteReadyColumn;
+export default React.memo(QuoteReadyColumn);
