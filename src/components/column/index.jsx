c5d1e66627d0dd04f862ed2cd3e7cc8a5aa84c39
Felipe Pinheiro <felipe.pinheiro.90@hotmail.com>
2019-08-10 02:18:41 -0300

    Add a minimum width to columns

--- src/components/column/index.jsx
+++ src/components/column/index.jsx
@@ -1,18 +1,19 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import Container from './container';
 import ItemContainer from './item-container';
 import ItemsContainer from './items-container';
 
 const Column = ({ header, items, renderItem }) => (
-  <>
+  <Container>
     {header}
     <ItemsContainer>
       {items.map(item => (
         <ItemContainer key={item.id}>{renderItem(item)}</ItemContainer>
       ))}
     </ItemsContainer>
-  </>
+  </Container>
 );
 
 Column.propTypes = {
