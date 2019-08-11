637c00e9146d7afe879c0dc73614156c9268b4be
Felipe Pinheiro <felipe.pinheiro.90@hotmail.com>
2019-08-11 10:58:17 -0300

    Add a transition when adding/removing items from column

--- src/components/column/index.jsx
+++ src/components/column/index.jsx
@@ -1,20 +1,31 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { useTransition } from 'react-spring';
 
 import Container from './container';
 import ItemContainer from './item-container';
 import ItemsContainer from './items-container';
 
-const Column = ({ header, items, renderItem }) => (
-  <Container>
-    {header}
-    <ItemsContainer>
-      {items.map(item => (
-        <ItemContainer key={item.id}>{renderItem(item)}</ItemContainer>
-      ))}
-    </ItemsContainer>
-  </Container>
-);
+const Column = ({ header, items, renderItem }) => {
+  const transitions = useTransition(items, item => item.id, {
+    from: { opacity: 0, transform: 'translateX(-16px)' },
+    enter: { opacity: 1, transform: 'translateX(0)' },
+    leave: { opacity: 0, transform: 'translateX(16px)' },
+  });
+
+  return (
+    <Container>
+      {header}
+      <ItemsContainer>
+        {transitions.map(({ item, props, key }) => (
+          <ItemContainer key={key} style={props}>
+            {renderItem(item)}
+          </ItemContainer>
+        ))}
+      </ItemsContainer>
+    </Container>
+  );
+};
 
 Column.propTypes = {
   header: PropTypes.node.isRequired,
--- src/components/column/item-container.js
+++ src/components/column/item-container.js
@@ -1,6 +1,7 @@
+import { animated } from 'react-spring';
 import styled from 'styled-components';
 
-export default styled.li`
+export default styled(animated.li)`
   &:nth-child(n + 2) {
     margin-top: 12px;
   }
